0ab0c3968499f8c0b53d1757222f4eebf44bbe8f
Maria Sanage <65598195+MariaS202@users.noreply.github.com>
2025-01-21 10:17:46 +0300

    navigation bottom tab updated

--- App.js
+++ App.js
@@ -1,71 +1,63 @@
 import { NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Calendar from './Calendar'
 import Settings from "./Settings";
 import Focus from './Focus'
 import Labels from './Labels'
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Login from './Login'
 import Home from './Home';
 
 export default function App(){
 
-    const CalendarStack = createStackNavigator()
-    const SettingsStack = createStackNavigator()
-    const FocusStack = createStackNavigator()
-    const LabelsStack = createStackNavigator()
-    const HomeStack = createStackNavigator()
     const MyTabs = createBottomTabNavigator()
     const Stack = createStackNavigator();
-    
-    function CalenderScreen() {
-        return(
-            <CalendarStack.Navigator screenOptions={{headerShown: false}}> 
-                <CalendarStack.Screen name="Calendar Screen" component={Calendar}/>
-            </CalendarStack.Navigator>
-        );
-    }
-    function SettingsScreen() {
-        return(
-            <SettingsStack.Navigator screenOptions={{headerShown: false}}> 
-                <SettingsStack.Screen name="Settings Screen" component={Settings}/>
-            </SettingsStack.Navigator>
-        );
-    }
-    function FocusScreen() {
-        return(
-            <FocusStack.Navigator screenOptions={{headerShown: false}}> 
-                <FocusStack.Screen name="Focus Screen" component={Focus}/>
-            </FocusStack.Navigator>
-        );
-    }
-    function LabelsScreen() {
-        return(
-            <LabelsStack.Navigator screenOptions={{headerShown: false}}> 
-                <LabelsStack.Screen name="Labels Screen" component={Labels}/>
-            </LabelsStack.Navigator>
-        );
-    }
-    function HomeScreen() {
-        return(
-            <HomeStack.Navigator screenOptions={{headerShown: false}}> 
-                <HomeStack.Screen name="Home Screen" component={Home}/>
-            </HomeStack.Navigator>
-        );
-    }
+
     function NavigationRoot() {
         return(
             <MyTabs.Navigator screenOptions={{headerShown: false}} initialRouteName='Home'>
-                <MyTabs.Screen name="Calendar" component={CalenderScreen}/>
-                <MyTabs.Screen name="Labels" component={LabelsScreen}/>
-                <MyTabs.Screen name="Home" component={HomeScreen} />
-                <MyTabs.Screen name="Focus" component={FocusScreen}/>
-                <MyTabs.Screen name="Settings" component={SettingsScreen}/>
-                
+                <MyTabs.Screen name="Calendar" component={Calendar} 
+                    options={{
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialCommunityIcons name='calendar-month' color={color} size={size} />
+                        )
+                    }}
+                />
+                <MyTabs.Screen name="Labels" component={Labels}
+                    options={{
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialCommunityIcons name='label' color={color} size={size} />
+                        )
+                    }}
+                />
+                <MyTabs.Screen name="Home" component={Home} 
+                    options={{
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialCommunityIcons name='home' color={color} size={size} />
+                        )
+                    }}
+                />
+                <MyTabs.Screen name="Focus" component={Focus}
+                    options={{
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialCommunityIcons name='flower-tulip' color={color} size={size} />
+                        )
+                    }}
+                />
+                <MyTabs.Screen name="Settings" component={Settings} 
+                    options={{
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialCommunityIcons name='tune' color={color} size={size} />
+                        )
+                    }}
+                />
+
             </MyTabs.Navigator>
         );
     }
-    
+   
+
     return(
         <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false}}>
@@ -75,5 +67,3 @@ export default function App(){
         </NavigationContainer>
     );
 }
-
-
