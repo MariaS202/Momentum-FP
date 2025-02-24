99a20dcd5c9b773e0db3593e43a916d2a4972e0c
Maria Sanage <65598195+MariaS202@users.noreply.github.com>
2025-02-24 14:16:10 +0300

    Update Context.js

--- Context.js
+++ Context.js
@@ -4,12 +4,16 @@ export const TasksContext = createContext();
 
 export const TasksProvider = ({ children }) => {
   const [tasks, setTasks] = useState([]);
+  const [focusTasks, setFocusTasks] = useState([]);
   const [labels, setLabels] = useState([])
   const [value, setValue] = useState('1')
   const [labelName, setLabelName] = useState('')
+  const [name, setName] = useState(null)
+
+
 
   return (
-    <TasksContext.Provider value={{ tasks, setTasks, labels, setLabels, value, setValue, labelName, setLabelName }}>
+    <TasksContext.Provider value={{ tasks, setTasks, labels, setLabels, value, setValue, labelName, setLabelName, focusTasks, setFocusTasks, name, setName }}>
       {children}
     </TasksContext.Provider>
   );
